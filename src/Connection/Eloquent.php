b293d3fb539e135e5e4f1ce7751f2db20ff8cb71
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-02-09 08:32:58 +0100

    fix namespace

--- src/Connection/Eloquent.php
+++ src/Connection/Eloquent.php
@@ -19,7 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace Fusio\Adapter\Laravel;
+namespace Fusio\Adapter\Laravel\Connection;
 
 use Fusio\Engine\ConnectionInterface;
 use Fusio\Engine\Form\BuilderInterface;
